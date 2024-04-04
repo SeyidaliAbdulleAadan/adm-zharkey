d5d8103cf7b63e19bc910a3a0a7a8ed2e28007f2
SeyidaliAbdulleAadan <165974847+SeyidaliAbdulleAadan@users.noreply.github.com>
2024-04-04 12:16:18 +0300

    ahmed
    waa folderka filasha

---
+++ AboutMe.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import MyPic from "./assets/my pic.jpg";
+
+
+const AboutMe = () => {
+  return (
+    <>
+      <div className="main-Body">
+      <div className="right-About">
+         
+          <img src="seyid.jpg" alt="" />
+        </div>
+        <div className="left-About">
+          <h1>About Me</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil ex
+            doloremque quis, enim reprehenderit illum voluptatum odio debitis
+            quas consectetur voluptates illo atque quam quia ullam dolorum
+            officia hic earum vitae voluptate quod recusandae distinctio
+            doloribus. Ducimus voluptatem, unde sint atque officia quis tenetur
+            eligendi. Suscipit repudiandae inventore error debitis repellat
+            asperiores magnam dolores dignissimos labore? Minus laboriosam
+            perspiciatis, cumque rem excepturi atque possimus laborum ab at sed
+            vel saepe unde repudiandae quam velit? Ducimus accusamus libero ex
+            explicabo, provident dignissimos beatae ab. Autem qui quo molestias,
+            eaque ad quod sed, ullam unde earum animi explicabo illo magnam
+            optio itaque.
+          </p>
+        </div>
+  
+      </div>
+    </>
+  );
+};
+
+export default AboutMe;
---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import AboutMe from "./AboutMe";
+import Footer from "./Footer";
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <AboutMe />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
